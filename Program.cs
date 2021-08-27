3705c39813c9b93548b8575eb2c610fbfd9895f5
biaaimi <biagioni.aimi@gmail.com>
2021-08-27 18:19:25 -0300

    exercicio arte

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace exercicios_etec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".========================================================================. ");
+            Console.WriteLine(" |        F   `-._                                                       J| ");
+            Console.WriteLine(" |       J        `-._                                                   F|");
+            Console.WriteLine(" |       |       J    `-.                                                L| ");
+            Console.WriteLine(" |--...__|_      |     F `-.                                            J | ");
+            Console.WriteLine(" |       F ```-- L..  J    J`-._                                        | | ");
+            Console.WriteLine(" |      J       J   `-|-.. |   J`-.___                                  L |");
+            Console.WriteLine(" |      J       |     |   `|--.|__|   ````````------......_______...--- F |");
+            Console.WriteLine(" |      |       |     |    |   |  F                                    J  |");
+            Console.WriteLine(" |```---|--.... L.    L    L   | J                                     |  |");
+            Console.WriteLine(" |      |      J  ```J----J-...L_|                                     |  |");
+            Console.WriteLine(" |      F      J     |    J   J  |       _..__    --======..______     |  | ");
+            Console.WriteLine(" |      L      J     |    |   |  |      /'    /     |/ / /:   ||       '  |");
+            Console.WriteLine(" |------L----..|.....J....|__ L_ F      L.____)     |/ / /:   ||       |  |");
+            Console.WriteLine(" |     J       |     |    |  J  J       (-(- c/ -.  |/ / /:   ||       |  |");
+            Console.WriteLine(" |     |       |     L    L  |__|       /    |    `-. ___ :   ||       F  |");
+            Console.WriteLine(" |     |       L  __J.---J'''|  |        |.'        |   )___|'            |");
+            Console.WriteLine(" |     |__..--J-''  |    |  _|-''_...  __|            /              J    |");
+            Console.WriteLine(" |---''F      |     |    |-'_:-'' _. ,'   |  -._.|  /             ,       | ");
+            Console.WriteLine(" |     L      |     L_.-':-'  _.-'  /     |      |    |             |     | ");
+            Console.WriteLine(" |    J       L _.-J  .-'  .-'   _ /    )       .`.   )  _          |     |");
+            Console.WriteLine(" |    |     .J-'   :-'   .'.--.   /    /     a#--'   |  `   ```----...'   |");
+            Console.WriteLine(" |    | _.-' |   .'    .'  Cc_ `-'  _.'   (U/      |  |             |     |");
+            Console.WriteLine(" |-_.-:'     | .'    .'       `L _)/     `-._J      .|  :             J   |");
+            Console.WriteLine(" |'   |      |(    .'   ___    `._J     . /  |   `./ L  J>             L  |");
+            Console.WriteLine(" |    |      |`.  (   ____  . .  _.  ) . /   L   _/  `--'              J  |");
+            Console.WriteLine(" |    |    .-'  |.`    ___     ._ -' . /    (    (                    `L  |");
+            Console.WriteLine(" |    | .-'      `.`-._      `-._   .' /      /  J                     `  |");
+            Console.WriteLine(" |    .'          )    `-._  ._ _.-' .'        )  )                     ` |");
+            Console.WriteLine(" | .-'           (_..--..  `-..___.-'         / ` ))                      |");
+            Console.WriteLine(" |                       `-.      .'       ,-'/,  /                       |");
+            Console.WriteLine(" |                          `.   /         C/juu,'                        |");
+            Console.WriteLine(" |                             / |                                        |");
+            Console.WriteLine(" |                            (_)                   ._                    | ");
+            Console.WriteLine(" |                                                 F  ` -..__             |");
+            Console.WriteLine(" |                                                J         | `-- .__     |");
+            Console.WriteLine(" |                                                F  `--.. _F       |`--..|");
+            Console.WriteLine(" |                                               J         J `--..__|_    |");
+            Console.WriteLine(" `a:f====================================================================='");
+            
+        }
+    }
+}
